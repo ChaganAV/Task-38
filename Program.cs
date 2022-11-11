b84e5b3405b3fe2b2b310856eac250cafb2cdd5f
Alexandr <jwchaganav@gmail.com>
2022-11-11 15:38:16 +0400

    double

--- Program.cs
+++ Program.cs
@@ -1,31 +1,34 @@
-﻿int size = 5;
-double[] arrNum = new double[size];
-double minRnd = 0;
-double maxRnd = 10;
-double minElem = 0;
-double maxElem = 0;
-Random rnd = new Random();
-
-for (int i = 0; i < arrNum.Length; i++)
+﻿internal class Program
 {
-    arrNum[i] = rnd.Next(minRnd,maxRnd);
-}
-PrintArray(arrNum);
-
-/*for(int i = 0; i < arrNum.Length; i++)
-{
-    if(i%2 == 0)
-        sumNegative+= arrNum[i];
-}
-Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях  {sumNegative}");*/
-
-// Functions
-void PrintArray(double[] array)
-{
-    for(int i = 0; i < array.Length; i++)
+    private static void Main(string[] args)
     {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+        int size = 5;
+        double[] arrNum = new double[size];
+        double minElem = 0;
+        double maxElem = 0;
+        Random rnd = new Random();
 
+        for (int i = 0; i < arrNum.Length; i++)
+        {
+            arrNum[i] = rnd.NextDouble();
+        }
+        PrintArray(arrNum);
+
+        /*for(int i = 0; i < arrNum.Length; i++)
+        {
+            if(i%2 == 0)
+                sumNegative+= arrNum[i];
+        }
+        Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях  {sumNegative}");*/
+
+        // Functions
+        void PrintArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
